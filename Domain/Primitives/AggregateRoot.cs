dc68d37b14f7f40b6aa2750f62510b1c4abbfd61
melii5 <melito2719bl@gmail.com>
2023-06-26 08:12:29 -0600

    Agregar archivos de proyecto.

---
+++ Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Primitives
+{
+    public abstract class AggregateRoot
+    {
+        private readonly List<DomainEvent> _events = new();
+        public ICollection<DomainEvent> GetDomainEvents() => _events;
+
+        protected void Raise(DomainEvent _event)
+        {
+            _events.Add(_event);
+        }
+    }
+}
