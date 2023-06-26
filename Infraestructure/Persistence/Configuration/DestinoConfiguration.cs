8bdfd9096357abac20271de66f9ada8583403a48
melii5 <melito2719bl@gmail.com>
2023-06-26 17:24:34 -0600

    creando migraciones

---
+++ Infraestructure/Persistence/Configuration/DestinoConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Destinos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Persistence.Configuration
+{
+    public class DestinoConfiguration : IEntityTypeConfiguration<Destinos>
+    {
+        public void Configure(EntityTypeBuilder<Destinos> builder)
+        {
+            builder.ToTable("destino");
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).HasConversion( destinoId=>destinoId.value,
+                value=> new DestinoId(value));
+            builder.Property(c => c.nombre).HasMaxLength(255);
+            builder.Property(c => c.descripcion).HasMaxLength(255); 
+            builder.Property(c=> c.ubicacion).HasMaxLength(255);
+            builder.Property(c => c.activo).IsRequired(true);
+        }
+    }
+}
---
+++ Infraestructure/Persistence/Configuration/PaqueteConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Paquetes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Persistence.Configuration
+{
+    public class PaqueteConfiguration : IEntityTypeConfiguration<Paquetes>
+    {
+        public void Configure(EntityTypeBuilder<Paquetes> builder)
+        {
+            builder.ToTable("paquete");
+            builder.HasKey(c=>c.Id);
+            builder.Property(c => c.Id).HasConversion(paqueteId => paqueteId.value,
+                value => new PaqueteId(value));
+            builder.Property(c => c.nombre).HasMaxLength(150);
+            builder.Property(c => c.descripcion).HasMaxLength(255);
+            builder.Property(c => c.fecha).IsRequired(true);
+            builder.Property(c => c.precio);
+            builder.Property(c => c.activo);
+
+        }
+    }
+}
